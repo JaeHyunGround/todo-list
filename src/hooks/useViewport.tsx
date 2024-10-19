d034a770f3660acc8653ceb172d45e396db2fcb1
JaeHyunGround <gothddlek@naver.com>
2024-10-19 21:43:56 +0900

    ✨feat: 뷰포트 너비를 구하기 위한 useViewport hook 구현
    반응형 디자인을 위해 사용자의 뷰포트 너비를 구하는 hook을 구현했습니다.

---
+++ src/hooks/useViewport.tsx
@@ -0,0 +1,20 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+
+export function useViewport() {
+  const [viewportWidth, setViewportWidth] = useState(0);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setViewportWidth(window.innerWidth);
+    };
+
+    handleResize();
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  return { viewportWidth };
+}

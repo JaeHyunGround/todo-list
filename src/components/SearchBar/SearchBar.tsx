5deac855ef046e7bcecbecf8354595299cda5bdc
JaeHyunGround <gothddlek@naver.com>
2024-10-22 19:30:03 +0900

    ♻️refactor: 입력값 없이 Todo를 추가하는 것을 방지하기 위해 required 타입 설정

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -6,24 +6,33 @@ import { SearchBarContainer, SearchBarForm } from './SearchBar.style';
 import { useViewport } from '@/hooks/useViewport';
 import { SearchBarProps } from './SearchBar.type';
 import { useFormContext } from 'react-hook-form';
+import useTodoStore from '@/stores/todoStore';
 
 const SearchBar = ({ handleSearchSubmit, REGISTER }: SearchBarProps) => {
   const { register, handleSubmit } = useFormContext();
+  const todos = useTodoStore((state) => state.todos);
 
   const { viewportWidth } = useViewport();
 
   return (
     <SearchBarContainer>
       <SearchBarForm onSubmit={handleSubmit(handleSearchSubmit)}>
-        <input placeholder="할 일을 입력해주세요." {...register(REGISTER)} />
+        <input
+          placeholder="할 일을 입력해주세요."
+          {...register(REGISTER, {
+            required: true,
+          })}
+        />
       </SearchBarForm>
       <Button
         width={viewportWidth > breakpoints.mobile ? '168px' : '52px'}
         height="52px"
         text={viewportWidth > breakpoints.mobile ? '추가하기' : ''}
-        color="var(--white)"
-        background="var(--violet-600)"
-        icon="plus-white"
+        color={todos.length === 0 ? 'var(--white)' : 'var(--slate-900)'}
+        background={
+          todos.length === 0 ? 'var(--violet-600)' : 'var(--slate-200)'
+        }
+        icon={todos.length === 0 ? 'plus-white' : 'plus-black'}
         buttonIconSize={16}
         borderRadius="50px"
         borderShadow={true}

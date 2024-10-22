39df75e87a4ec2f305f2b7889484b3b248730e4c
JaeHyunGround <gothddlek@naver.com>
2024-10-22 19:20:15 +0900

    ✨feat: todo 데이터 요청, 추가, 수정을 위한 API 로직 작성

---
+++ src/apis/todoApi.ts
@@ -0,0 +1,23 @@
+import { PatchTodoItem, PostTodoItem } from '@/types';
+import axios from 'axios';
+
+export const fetchTodos = async () => {
+  const response = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/items`);
+  return response.data;
+};
+
+export const addTodo = async (todo: PostTodoItem) => {
+  const response = await axios.post(
+    `${process.env.NEXT_PUBLIC_API_URL}/items`,
+    todo
+  );
+  return response.data;
+};
+
+export const patchTodo = async (id: number, todo: PatchTodoItem) => {
+  const response = await axios.patch(
+    `${process.env.NEXT_PUBLIC_API_URL}/items/${id}`,
+    todo
+  );
+  return response.data;
+};

96298fec6c6f9381218751492b30a546f9d41046
JaeHyunGround <gothddlek@naver.com>
2024-10-23 18:40:27 +0900

    ♻️refactor: 컴포넌트 export문 작성

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,4 @@
 export { default as Header } from './Header/Header';
 export { default as TodoList } from './TodoList/TodoList';
+export { default as ImageUploader } from './ImageUploader/ImageUploader';
+export { default as Memo } from './Memo/Memo';

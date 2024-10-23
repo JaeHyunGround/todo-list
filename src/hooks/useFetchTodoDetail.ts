ee376040a32be7de00d06788fe695b8bd9321c09
JaeHyunGround <gothddlek@naver.com>
2024-10-23 18:37:21 +0900

    ♻️refactor: useFetchTodoDetail 쿼리문 수정

--- src/hooks/useFetchTodoDetail.ts
+++ src/hooks/useFetchTodoDetail.ts
@@ -2,7 +2,7 @@ import { fetchTodoDetail } from '@/apis/todoApi';
 import { useQuery } from 'react-query';
 
 export const useFetchTodoDetail = (todoId: number) => {
-  return useQuery(['todo', todoId], () => fetchTodoDetail(todoId), {
+  return useQuery(`${todoId}`, () => fetchTodoDetail(todoId), {
     enabled: !!todoId,
     refetchOnWindowFocus: false,
     staleTime: 60000,
